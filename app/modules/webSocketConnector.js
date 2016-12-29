33ea6018a353ecc0b053a3b2d4b0da87e1abd99b
Matti Suur-Askola <matti.suur-askola@siili.com>
2016-12-29 23:09:43 +0200

    cleanup linting problems

--- app/modules/webSocketConnector.js
+++ app/modules/webSocketConnector.js
@@ -1,32 +1,28 @@
 class WebSocketConnector {
 
-    listeners = [];
-
     constructor() {
         this.websocketHost = location.origin.replace(/^http/, 'ws');
         this.connect();
+        this.listeners = [];
     }
 
     handleClose() {
-        console.log('connection lost to ws..')
         setTimeout(this.connect.bind(this), 5000);
     }
-	handleError() {
-		console.log("Error : " + evt.toString());
-	}
+    handleError() {
+    }
 
     connect() {
-        console.log('connecting ws..')
+        // console.log('connecting ws..');
         this.wsConnection = new WebSocket(`${this.websocketHost}/ws/controlMessages`);
-        this.wsConnection.onmessage = this.notify.bind(this)
+        this.wsConnection.onmessage = this.notify.bind(this);
         this.wsConnection.onclose = this.handleClose.bind(this);
         this.wsConnection.onError = this.handleError.bind(this);
     }
 
     send(message) {
         if (typeof message === 'object') {
-//            console.log(JSON.stringify(message));
-            return this.wsConnection.send(JSON.stringify(message));
+            this.wsConnection.send(JSON.stringify(message));
         }
     }
     notify(event) {
@@ -40,7 +36,7 @@ class WebSocketConnector {
     }
     unsubscripe(listener) {
         const index = this.listeners.indexOf(listener);
-        if(index => 0 ) {
+        if (index >= 0) {
             this.listener.splice(index, 1);
         }
     }
